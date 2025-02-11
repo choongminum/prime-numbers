86a9516fe34f1fbff6935e386de4757f474cfca3
Choong Min Um <choongmin.um@unh.edu>
2025-02-11 17:14:07 -0500

    fix: add `if` statement to ensure `num1` != 1

--- prime.py
+++ prime.py
@@ -8,14 +8,17 @@ import argparse
 
 
 def find_prime(num1, num2):
-    for n in range(num1, num2 + 1):
-        for x in range(2, n):
-            if n % x == 0:
-                print(n, "is equal to", x, "*", n // x)
-                break
-        else:
-            # loop fell through without finding a factor
-            print(n, "is a prime number")
+    if num1 == 1:
+        print("The first number must be larger than 1!")
+    else:
+        for n in range(num1, num2 + 1):
+            for x in range(2, n):
+                if n % x == 0:
+                    print(n, "is equal to", x, "*", n // x)
+                    break
+            else:
+                # loop fell through without finding a factor
+                print(n, "is a prime number")
 
 
 def def_args():
